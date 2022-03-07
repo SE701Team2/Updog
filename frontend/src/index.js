3d894e07f0ae032413665af1e1c9be427f182b00
nzbasic <jcop589@aucklanduni.ac.nz>
2022-03-08 12:21:36 +1300

    chore(fe): add timeago

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+TimeAgo.addDefaultLocale(en);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
