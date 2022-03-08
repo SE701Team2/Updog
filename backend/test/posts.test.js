86270660be00bb7fd7729f59b085887153e658db
dj5822 <Dj5822@gmail.com>
2022-03-09 10:32:34 +1300

    Added comments to test file

--- backend/test/posts.test.js
+++ backend/test/posts.test.js
@@ -5,6 +5,7 @@ describe('POST /posts', () => {
 
     describe('when creating a valid post', () => {})
 
+    // the parent must exist for it to be a valid post.
     describe('when creating an invalid post', () => {})
 })
 
@@ -21,8 +22,12 @@ describe('PUT /posts', () => {
 
     describe('when the post id can not be found', () => {})
 
-    describe('when modifying a valid post', () => {})
+    describe('when modifying a post in a valid way', () => {})
 
+    /* 
+    the parent must exist for it to be a valid post.
+    the datatypes for the other message body fields must be correct.
+    */
     describe('when modifying a post in an invalid way', () => {})
 })
 
@@ -35,5 +40,8 @@ describe('DELETE /posts', () => {
 
     describe('when deleting a leaf post', () => {})
 
+    /*
+    Child posts need to get deleted along with the parent post.
+    */
     describe('when deleting a parent post', () => {})
 })
