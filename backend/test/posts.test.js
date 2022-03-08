293a246f189537bca43490729168cc1c9f688a66
dj5822 <Dj5822@gmail.com>
2022-03-09 09:57:58 +1300

    Testing plan

---
+++ backend/test/posts.test.js
@@ -0,0 +1,39 @@
+describe('POST /posts', () => {
+    describe('when not authenticated', () => {})
+
+    describe('when auth token is invalid', () => {})
+
+    describe('when creating a valid post', () => {})
+
+    describe('when creating an invalid post', () => {})
+})
+
+describe('GET /posts', () => {
+    describe('when the post id can not be found', () => {})
+
+    describe('when the post id can be found', () => {})
+})
+
+describe('PUT /posts', () => {
+    describe('when not authenticated', () => {})
+
+    describe('when auth token is invalid', () => {})
+
+    describe('when the post id can not be found', () => {})
+
+    describe('when modifying a valid post', () => {})
+
+    describe('when modifying a post in an invalid way', () => {})
+})
+
+describe('DELETE /posts', () => {
+    describe('when not authenticated', () => {})
+
+    describe('when auth token is invalid', () => {})
+
+    describe('when the post id can not be found', () => {})
+
+    describe('when deleting a leaf post', () => {})
+
+    describe('when deleting a parent post', () => {})
+})
