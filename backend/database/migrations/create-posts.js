c9004b05ddac8eb3ec7b008060766a112412f49d
dj5822 <Dj5822@gmail.com>
2022-03-08 22:55:48 +1300

    Modified the posts table in the database.

--- backend/database/migrations/create-posts.js
+++ backend/database/migrations/create-posts.js
@@ -1,19 +1,33 @@
-'use strict';
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable('posts', {
+    up: (queryInterface, Sequelize) =>
+        queryInterface.createTable('posts', {
             id: {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
-                type: Sequelize.INTEGER
+                type: Sequelize.INTEGER,
             },
             text_content: {
-                type: Sequelize.STRING
-            }
-        });
-    },
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable('posts');
-    }
-};
+                type: Sequelize.STRING,
+            },
+            author: {
+                type: Sequelize.STRING,
+            },
+            parent: {
+                type: Sequelize.INTEGER,
+            },
+            usersLiked: {
+                type: Sequelize.INTEGER,
+            },
+            usersShared: {
+                type: Sequelize.INTEGER,
+            },
+            timestamp: {
+                type: Sequelize.DATE,
+            },
+            attachments: {
+                type: Sequelize.STRING,
+            },
+        }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('posts'),
+}
