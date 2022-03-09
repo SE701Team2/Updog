de40869e47b167d3ea5fe5306a49bcad0e8bf40d
dj5822 <Dj5822@gmail.com>
2022-03-09 15:08:47 +1300

    Added tests for POST /posts

--- backend/test/posts.test.js
+++ backend/test/posts.test.js
@@ -1,12 +1,73 @@
+import request from 'supertest'
+import server from '../server/index'
+import models from '../database/models'
+import { Authentication } from '../middlewares/authentication'
+
 describe('POST /posts', () => {
-    describe('when not authenticated', () => {})
+    describe('when not authenticated', () => {
+        it('should return response code of 400', async () => {
+            const response = await request(server).post('/api/posts').send({
+                text_content: 'some random text',
+                parent: null,
+            })
+            expect(response.statusCode).toBe(400)
+        })
+    })
 
-    describe('when auth token is invalid', () => {})
+    describe('when creating a valid post', () => {
+        it('should return response code of 200', async () => {
+            const createUserResponse = await request(server)
+                .post('/api/users')
+                .send({
+                    username: 'testUser',
+                    email: 'testUser@testmail.com',
+                    password: 'password',
+                })
 
-    describe('when creating a valid post', () => {})
+            const authResponse = await request(server)
+                .post('/api/users/authenticate')
+                .send({
+                    id: createUserResponse.body.id,
+                })
+
+            const response = await request(server)
+                .post('/api/posts')
+                .set('Authorization', `Bearer ${authResponse.body.authToken}`)
+                .send({
+                    text_content: 'some random text 2',
+                    parent: null,
+                })
+            expect(response.statusCode).toBe(201)
+        })
+    })
 
     // the parent must exist for it to be a valid post.
-    describe('when creating an invalid post', () => {})
+    describe('when creating an invalid post', () => {
+        it('should return response code of 404 not found', async () => {
+            const createUserResponse = await request(server)
+                .post('/api/users')
+                .send({
+                    username: 'testUser',
+                    email: 'testUser@testmail.com',
+                    password: 'password',
+                })
+
+            const authResponse = await request(server)
+                .post('/api/users/authenticate')
+                .send({
+                    id: createUserResponse.body.id,
+                })
+
+            const response = await request(server)
+                .post('/api/posts')
+                .set('Authorization', `Bearer ${authResponse.body.authToken}`)
+                .send({
+                    text_content: 'some random text 2',
+                    parent: 99999,
+                })
+            expect(response.statusCode).toBe(404)
+        })
+    })
 })
 
 describe('GET /posts', () => {
