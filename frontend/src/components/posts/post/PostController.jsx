fab4d290fd2ffe0c1fc3a054042d2584c78e8263
James <54062686+nzbasic@users.noreply.github.com>
2022-03-09 18:46:57 +1300

    [#44] - Add optional post data input to posts

--- frontend/src/components/posts/post/PostController.jsx
+++ frontend/src/components/posts/post/PostController.jsx
@@ -1,28 +1,29 @@
 // eslint-disable-next-line import/no-cycle
 import PostView from "./PostView"
-import { sampleReply, samplePost } from './mock-data'
+import replyData from './mock-data'
 
 /**
- * Creates a post
- * @prop {number} id 
+ * Creates a post. One of either id or data must be provided
+ * @prop {number} id - optional, data will be fetched using the id
+ * @prop {object} data - optional, use this post data to render the post
  * @prop {boolean} condensed - optional, makes the post take up less space
  * @prop {boolean} showReplies - optional, also renders each 1st level reply to the post
- * @prop {boolean} isReply - optional, used internally to mock data, can be removed later
  */
-const PostController = ({ id, condensed = false, showReplies = false, isReply = false }) => {
-    // add logic to fetch post from the id.
-    let data = samplePost
-    data.id = id
-
-    // for mock purposes only
-    if (isReply) {
-        data = sampleReply
+const PostController = ({ id = 0, data = null, condensed = false, showReplies = false }) => {
+    let postData = data
+    if (id) {
+        // add logic to fetch post from the id.
+        // for mock, we assume that if an id is given its a reply 
+        postData = replyData
+    } else if (!data) {
+        // true if neither id or data is given
+        return <div>Error retrieving post data</div>
     }
 
     return (
         <PostView 
-            condensed={condensed || isReply} 
-            postData={data} 
+            condensed={condensed} 
+            postData={postData} 
             showReplies={showReplies}
         />
     )
