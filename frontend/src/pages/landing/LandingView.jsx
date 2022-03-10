f152430694273efca1704db12423c5e400e194db
Michael Shaimerden <michael@tadesign.co.nz>
2022-03-10 14:42:09 +1300

    Add landing page draft

--- frontend/src/pages/landing/LandingView.jsx
+++ frontend/src/pages/landing/LandingView.jsx
@@ -1,9 +1,42 @@
-import classes from "./landing.module.scss";
+import Button from '@mui/material/Button'
+import classes from './landing.module.scss'
 
-const LandingView = () => (
-  <div className={classes.container}>
-    <h1>Updog</h1>
-  </div>
-);
+const LandingView = ({ onLogin, onSignUp }) => (
+    <>
+        <div className={classes.logo}>Logo</div>
+        <div className={classes.container}>
+            <div className={classes.slogan}>
+                Check out whats going on
+                <br /> in the world right now.
+            </div>
+            <div className={classes.buttonsContainer}>
+                <div className={classes.loginButton}>
+                    <Button
+                        variant="outlined"
+                        onClick={onLogin}
+                        fullWidth
+                        style={{ borderRadius: 100 }}
+                    >
+                        Login
+                    </Button>
+                </div>
+                <span className={classes.divider}>Or</span>
+                <div className={classes.createAccount}>
+                    <Button
+                        variant="contained"
+                        onClick={onSignUp}
+                        fullWidth
+                        style={{ borderRadius: 100 }}
+                    >
+                        Create an Account
+                    </Button>
+                </div>
+            </div>
+            <div className={classes.loginText}>
+                Have an account already? Log in
+            </div>
+        </div>
+    </>
+)
 
 export default LandingView
