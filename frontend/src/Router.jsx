7bb77ed3d72811bcd23264e909da19d5e8e13e38
Michael Shaimerden <michael@tadesign.co.nz>
2022-03-10 14:41:28 +1300

    Separate authenticated routes

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,11 +2,30 @@ import { Route, Routes } from 'react-router-dom'
 import Landing from './pages/landing/LandingController'
 import Post from './pages/post/PostPageController'
 
-const Router = () => (
-    <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/post/:id" element={<Post />} />
-    </Routes>
-)
+const Router = () => {
+    // fetch the token to check if the user is authenticated
+
+    let routes
+
+    const token = localStorage.getItem('token')
+
+    if (token) {
+        // Auth routes
+        routes = (
+            <Routes>
+                <Route path="/post/:id" element={<Post />} />
+            </Routes>
+        )
+    } else {
+        // Unauth routes
+        routes = (
+            <Routes>
+                <Route path="/" element={<Landing />} />
+            </Routes>
+        )
+    }
+
+    return routes
+}
 
 export default Router
