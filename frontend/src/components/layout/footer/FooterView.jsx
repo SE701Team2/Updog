f1579d55de174450a371fbe7b46a30d3b952303c
asok3781 <61459012+asok3781@users.noreply.github.com>
2022-03-10 19:09:19 +1300

    Added basic styling to MUI bottom nav

--- frontend/src/components/layout/footer/FooterView.jsx
+++ frontend/src/components/layout/footer/FooterView.jsx
@@ -1,23 +1,28 @@
 import classes from './footer.module.scss'
-import { BottomNavigation } from '@mui/material';
-import { BottomNavigationAction } from '@mui/material';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { StyledEngineProvider } from '@mui/material';
+
 const FooterView = () => (
     <div className={classes.container}>
-        <BottomNavigation
-            showLabels
-            // value={value}
-            // onChange={(event, newValue) => {
-            //     setValue(newValue);
-            // }}
-            className='BottomNavigation'
-        >
-            <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-            <BottomNavigationAction label="Search" icon={<SearchIcon />} />
-            <BottomNavigationAction label="Notifications" icon={<NotificationsIcon />} />
-        </BottomNavigation>
+        <StyledEngineProvider injectFirst>
+            <BottomNavigation
+                showLabels
+                // value={value}
+                // onChange={(event, newValue) => {
+                //     setValue(newValue);
+                // }}
+                className={classes.nav}
+            >
+                <BottomNavigationAction className={classes.navIcon} label="Home" icon={<HomeIcon />} />
+                <BottomNavigationAction className={classes.navIcon} label="Search" icon={<SearchIcon />} />
+                <BottomNavigationAction className={classes.navIcon} label="Notifications" icon={<NotificationsIcon />} />
+            </BottomNavigation>
+        </StyledEngineProvider>
+
     </div>
 )
 
