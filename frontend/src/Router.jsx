a290d81867ab549e312c8ea0adaf982c6e92bab6
Michael Shaimerden <michael@tadesign.co.nz>
2022-03-10 14:41:28 +1300

    Separate authenticated routes

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -5,13 +5,33 @@ import Follows from './pages/follows/FollowsPageController'
 
 import Registration from './pages/registration/RegistrationPagesController'
 
-const Router = () => (
-    <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/post/:id" element={<Post />} />
-        <Route path="/signUp" element={<Registration />} />
-        <Route path="/user/:username/follows" element={<Follows />} />
-    </Routes>
-)
+const Router = () => {
+    // fetch the token to check if the user is authenticated
+
+    let routes
+
+    const token = localStorage.getItem('token')
+
+    if (token) {
+        // Auth routes
+        routes = (
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/signUp" element={<Registration />} />
+                <Route path="/user/:username/follows" element={<Follows />} />
+                <Route path="/post/:id" element={<Post />} />
+            </Routes>
+        )
+    } else {
+        // Unauth routes
+        routes = (
+            <Routes>
+                <Route path="/" element={<Landing />} />
+            </Routes>
+        )
+    }
+
+    return routes
+}
 
 export default Router
