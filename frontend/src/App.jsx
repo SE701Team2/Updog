954e3d81caab0e7decca71bcd3b69dea8eb331b8
James <54062686+nzbasic@users.noreply.github.com>
2022-03-11 13:10:49 +1300

    feat(fe): configure theme provider for MUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import classes from './app.module.scss';
+import palette from './styles/theme.scss'
 import Router from './Router'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: palette.primary,
+      light: palette.primaryVariant,
+    },
+    secondary: {
+      main: palette.secondary,
+      light: palette.secondaryVariant,
+    },
+    error: {
+      main: palette.error,
+    },
+  },
+});
 
 const App = () => (
     <div className={classes.container}>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
     </div>
   )
 
