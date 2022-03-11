b414fb6bde07ffd359e7c4d681b9593083924714
BJ <kelmeld@outlook.com>
2022-03-12 03:13:10 +1300

    add mock-follows with pfps so it works

--- frontend/src/pages/follows/mock-follows.js
+++ frontend/src/pages/follows/mock-follows.js
@@ -1,6 +1,17 @@
 const follows = [
-    { id: 21, username: 'upbot21', nickname: 'Updog21' },
-    { id: 22, username: 'upbot22', nickname: 'Updog22' },
+    {
+        id: 566,
+        username: 'poggies',
+        nickname: 'my little pogchamp',
+        profilePic: 'https://i.imgur.com/VAHLM0Q.jpeg',
+    },
+    {
+        id: 667,
+        username: 'cerealean',
+        nickname: ':cereal:',
+        profilePic:
+            'https://cdn.discordapp.com/emojis/827772639019532290.webp?size=96&quality=lossless',
+    },
     { id: 23, username: 'upbot23', nickname: 'Updog23' },
     { id: 24, username: 'upbot24', nickname: 'Updog24' },
     { id: 25, username: 'upbot25', nickname: 'Updog25' },
