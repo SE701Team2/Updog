741392face6b07f3ae2146ee13858795c2cf4f14
dj5822 <Dj5822@gmail.com>
2022-03-12 22:36:35 +1300

    Converted datatypes to integers

--- backend/database/models/likedpost.js
+++ backend/database/models/likedpost.js
@@ -1,4 +1,3 @@
-'use strict'
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
     class likedPost extends Model {
@@ -13,8 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     }
     likedPost.init(
         {
-            postId: DataTypes.DECIMAL,
-            userId: DataTypes.DECIMAL,
+            postId: DataTypes.INTEGER,
+            userId: DataTypes.INTERGER,
         },
         {
             sequelize,
--- backend/database/models/sharedpost.js
+++ backend/database/models/sharedpost.js
@@ -1,4 +1,3 @@
-'use strict'
 const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
     class sharedPost extends Model {
@@ -13,8 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     }
     sharedPost.init(
         {
-            postId: DataTypes.DECIMAL,
-            userId: DataTypes.DECIMAL,
+            postId: DataTypes.INTEGER,
+            userId: DataTypes.INTEGER,
         },
         {
             sequelize,
