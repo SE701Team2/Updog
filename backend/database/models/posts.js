c43745a641ba4420f9eac16d2213de93054b9642
Whan Jung <jung909@hotmail.com>
2022-03-13 03:44:11 +1300

    Temporary change to add attachment element for posts

--- backend/database/models/posts.js
+++ backend/database/models/posts.js
@@ -3,12 +3,29 @@ module.exports = (sequelize, DataTypes) => {
     const posts = sequelize.define(
         'posts',
         {
-            testContent: DataTypes.STRING
+            content: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            author: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                },
+                allowNull: false
+            },
+            parent: DataTypes.INTEGER,
+            attachments: DataTypes.STRING,
         },
-        {}
     );
     posts.associate = function(models) {
         // associations can be defined here
+        posts.belongsTo(models.users,
+            {
+                foreignKey: 'author',
+                targetKey: 'id'
+            });
     };
     return posts;
 };
