db20f0c1a6f2f2dbfd4b9c17d0d7a51b50c929b7
BJ <kelmeld@outlook.com>
2022-03-12 15:17:45 +1300

    fix getting username issue on follows

--- frontend/src/pages/follows/FollowsPageController.jsx
+++ frontend/src/pages/follows/FollowsPageController.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import { useParams } from "react-router-dom"
-import FollowsPageView from "./FollowsPageView"
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import FollowsPageView from './FollowsPageView'
 import followers from './mock-followers'
 import follows from './mock-follows'
 
@@ -9,23 +9,25 @@ import follows from './mock-follows'
  * a header and the navigation footer.
  */
 const FollowsPageController = () => {
-    // gets the id from the current url 
-    const { id } = useParams()
-    
+    // gets the username from the current url
+    const { username } = useParams()
+
     const [tab, setTab] = useState(0)
     const [followsData, setFollowsData] = useState(followers)
 
     const handleChange = (event, newTab) => {
         setTab(newTab)
-        setFollowsData(
-            newTab === 0 ? followers : follows
-        )
+        setFollowsData(newTab === 0 ? followers : follows)
     }
     // we are going to fetch followers from the user
-    parseInt(id, 10)
-    
 
-    return <FollowsPageView followsData={followsData}  tab={tab} handleChange={handleChange} />
+    return (
+        <FollowsPageView
+            followsData={followsData}
+            tab={tab}
+            handleChange={handleChange}
+        />
+    )
 }
 
 export default FollowsPageController
--- frontend/src/pages/follows/FollowsPageView.jsx
+++ frontend/src/pages/follows/FollowsPageView.jsx
@@ -1,31 +1,26 @@
-import { List, ListItem, Tabs, Tab } from '@mui/material';
+import { List, ListItem, Tabs, Tab } from '@mui/material'
 
 import classes from './followspage.module.scss'
 import Footer from '../../components/layout/footer/FooterController'
 import Header from '../../components/layout/header/HeaderController'
 import SimpleUserDetails from '../../components/user/simpledetails/SimpleUserDetailsController'
 
-
-
 const FollowsPageView = ({ followsData, tab, handleChange }) => (
-    
     <div className={classes.container}>
         <Header />
 
         <Tabs value={tab} onChange={handleChange} variant="fullWidth">
             <Tab label="Followers" />
-            <Tab label="Follows"/>
+            <Tab label="Follows" />
         </Tabs>
-        <div className={classes.pageContent} >
+        <div className={classes.pageContent}>
             <List>
-                {followsData.map((follower) => 
-                <ListItem key={follower.id} divider>
-                    <SimpleUserDetails user={follower} />
-                </ListItem>
-                )}
+                {followsData.map((follower) => (
+                    <ListItem key={follower.id} divider>
+                        <SimpleUserDetails user={follower} />
+                    </ListItem>
+                ))}
             </List>
-
-            
         </div>
         <Footer />
     </div>
