224b0726610a0807673eaff5a2c93be645ecd686
asok3781 <61459012+asok3781@users.noreply.github.com>
2022-03-12 16:44:16 +1300

    Ran prettier

--- frontend/src/components/layout/footer/FooterController.jsx
+++ frontend/src/components/layout/footer/FooterController.jsx
@@ -1,18 +1,23 @@
-import React from "react"
-import FooterView from "./FooterView"
+import React from 'react'
+import FooterView from './FooterView'
 /**
  * This is a reusable footer component intended for users to navigate to main pages.
  */
-const FooterController = () => {    
-    const path = window.location.pathname;
-    const [currentPage, setCurrentPage] = React.useState(path);
+const FooterController = () => {
+    const path = window.location.pathname
+    const [currentPage, setCurrentPage] = React.useState(path)
     // Mock variable for global notification number state
-    const [notificationsNumber] = React.useState(2);
+    const [notificationsNumber] = React.useState(2)
     const onPageChange = (event, newPage) => {
-        setCurrentPage(newPage);
+        setCurrentPage(newPage)
     }
-    return <FooterView currentPage={currentPage} onPageChange={onPageChange} notificationsNumber={notificationsNumber}  />
+    return (
+        <FooterView
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+            notificationsNumber={notificationsNumber}
+        />
+    )
 }
-    
 
 export default FooterController
