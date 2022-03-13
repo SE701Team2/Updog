83edc43212578634335143425c811ade0431d822
James <54062686+nzbasic@users.noreply.github.com>
2022-03-14 11:39:24 +1300

    ui tweak for consistency

--- frontend/src/pages/user/UserPageView.jsx
+++ frontend/src/pages/user/UserPageView.jsx
@@ -5,6 +5,7 @@ import Footer from '../../components/layout/footer/FooterController'
 import HeaderCustom from '../../components/layout/headercustom/HeaderCustomController'
 import ProfileUserDetails from '../../components/user/profiledetails/ProfileUserDetailsController'
 import Post from '../../components/posts/post/PostController'
+import Divider from '@mui/material/Divider'
 
 // eslint-disable-next-line no-unused-vars
 const UserPageView = ({ userData, userFeed, loggedIn }) => (
@@ -24,16 +25,17 @@ const UserPageView = ({ userData, userFeed, loggedIn }) => (
                 Placeholder
             </Button>
             <ProfileUserDetails user={userData} />
-            &nbsp;
-            <hr />
-            <h1>Posts</h1>
-            <List>
-                {userFeed?.map((post) => (
-                    <ListItem key={post.id} divider>
-                        <Post data={post} condensed />
-                    </ListItem>
-                ))}
-            </List>
+            
+            <div className={classes.posts}>
+                <h2>Activity</h2>
+                <List>
+                    {userFeed?.map((post) => (
+                        <ListItem key={post.id} sx={{ paddingLeft: 0, paddingRight: 0 }} divider>
+                            <Post data={post} condensed />
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
         </div>
         <Footer />
     </div>
