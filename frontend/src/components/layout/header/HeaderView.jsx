121199fe901a8815c57d7464d8ec97d13fe84765
Andy Kweon <63220455+skwe902@users.noreply.github.com>
2022-03-13 19:59:23 +1300

    Added logo

--- frontend/src/components/layout/header/HeaderView.jsx
+++ frontend/src/components/layout/header/HeaderView.jsx
@@ -4,21 +4,16 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import MenuIcon from '@mui/icons-material/Menu';
 import Avatar from '@mui/material/Avatar';
+import logo from '../../../images/logo.png'
 
 const HeaderView = ({goToFeed, goToProfile}) => (
     <div className={classes.container}>
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        color="inherit"
-                        sx={{ mr: 2 }}
-                        onClick={goToFeed}
-                    >
-                        <MenuIcon />
+                    <IconButton onClick={goToFeed} sx={{ mr: 2 }}>
+                        <img alt="logo" className={classes.photo} src={logo} />
                     </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Updog
