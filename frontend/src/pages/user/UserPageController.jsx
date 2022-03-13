9434b8f55764631a2af7ef4b4fa516d9f86f814f
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-03-13 18:14:44 +1300

    Add posts and button for follow

--- frontend/src/pages/user/UserPageController.jsx
+++ frontend/src/pages/user/UserPageController.jsx
@@ -6,10 +6,20 @@ import { sampleUser, sampleFeed } from './mock-data'
  * This page renders a user page
  */
 const UserPageController = () => {
+    // will be used when api call is implemented
     // eslint-disable-next-line no-unused-vars
     const { username } = useParams()
 
-    return <UserPageView userData={sampleUser} userFeed={sampleFeed} />
+    // once auth is implemented this will be removed
+    const logginState = true
+
+    return (
+        <UserPageView
+            userData={sampleUser}
+            userFeed={sampleFeed}
+            loggedIn={logginState}
+        />
+    )
 }
 
 export default UserPageController
--- frontend/src/pages/user/UserPageView.jsx
+++ frontend/src/pages/user/UserPageView.jsx
@@ -1,19 +1,38 @@
+import { List, ListItem } from '@mui/material'
+import Button from '@mui/material/Button'
 import classes from './userpage.module.scss'
 import Footer from '../../components/layout/footer/FooterController'
 import HeaderCustom from '../../components/layout/headercustom/HeaderCustomController'
 import ProfileUserDetails from '../../components/user/profiledetails/ProfileUserDetailsController'
+import Post from '../../components/posts/post/PostController'
 
 // eslint-disable-next-line no-unused-vars
-const UserPageView = ({ userData }, { userFeed }) => (
+const UserPageView = ({ userData, userFeed, loggedIn }) => (
     <div className={classes.container}>
-        <HeaderCustom />
+        <HeaderCustom title={userData.username} />
         <img
             className={classes.banner}
             alt="Profile Banner"
             src={userData.profileBanner}
         />
         <div className={classes.pageContent}>
+            <Button
+                variant="contained"
+                disableElevation
+                className={classes.followButton}
+            >
+                Follow
+            </Button>
             <ProfileUserDetails user={userData} />
+            &nbsp;
+            <h1>Posts</h1>
+            <List>
+                {userFeed?.map((post) => (
+                    <ListItem key={post.id} divider>
+                        <Post data={post} condensed />
+                    </ListItem>
+                ))}
+            </List>
         </div>
         <Footer />
     </div>
