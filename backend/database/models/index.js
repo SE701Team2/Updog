6c1b8551be782c563896b11024016e4d349b9c92
JL-2644 <67808129+JL-2644@users.noreply.github.com>
2022-03-14 18:31:00 +1300

    [#23] Generate API documentation (#88)
    * [#23] Generate api doc with swagger, fix eslint errors
    - The swagger api doc contains detailed information on our models and endpoints
    - eslint errors were caused by the rollout of global eslint and prettier configurations
    
    * [#23] Update readme for instructions on accessing swagger
    
    * [#23] Fix bug where it hangs after running all test suites

--- backend/database/models/index.js
+++ backend/database/models/index.js
@@ -1,39 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const { DB } = require('../../config/config');
-const config = DB;
-const db = {};
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+
+const basename = path.basename(__filename)
+const { DB } = require('../../config/default')
+
+const config = DB
+const db = {}
 
 const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    host: config.host,
-    dialect: config.dialect
-  }
-);
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect: config.dialect,
+    }
+)
 
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-    );
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+    .filter(
+        (file) =>
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js'
+    )
+    .forEach((file) => {
+        const model = sequelize.import(path.join(__dirname, file))
+        db[model.name] = model
+    })
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db)
+    }
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-module.exports = db;
+module.exports = db
