b6718d2f0f6929fe4ec4cd4027ecb911d88cc6eb
henrym25 <henry3399@hotmail.co.nz>
2022-03-15 00:22:09 +1300

    Added basic card rendering for each feed element

--- frontend/src/pages/dashboard/DashboardPageController.jsx
+++ frontend/src/pages/dashboard/DashboardPageController.jsx
@@ -1,5 +1,12 @@
 import DashboardPageView from './DashboardPageView'
+import sampleFeed from './mock-dashboard-data'
 
-const DashboardPageController = () => <DashboardPageView />
+/**
+ * This page renders a page which displays posts from user's followers. Each post is
+ * displayed in a condensed view. Refer to PostView Component for more details
+ */
+const DashboardPageController = () => (
+    <DashboardPageView feedData={sampleFeed} />
+)
 
 export default DashboardPageController
--- frontend/src/pages/dashboard/DashboardPageView.jsx
+++ frontend/src/pages/dashboard/DashboardPageView.jsx
@@ -1,10 +1,31 @@
+import { Card, styled } from '@mui/material'
+
+import classes from './dashboardpage.module.scss'
 import HeaderCustom from '../../components/layout/headercustom/HeaderCustomController'
 import Footer from '../../components/layout/footer/FooterController'
+import Post from '../../components/posts/post/PostController'
 
-const DashboardPageView = () => (
+/**
+ * Card container for each feed post
+ */
+const FeedCard = styled(Card)({
+    width: '90%',
+    margin: 'auto',
+    marginTop: '5px',
+    marginBottom: '10px',
+    padding: '0 10px 0 10px',
+})
+
+const DashboardPageView = ({ feedData }) => (
     <div>
         <HeaderCustom title="Feed" />
-
+        <div className={classes.feedCard}>
+            {feedData.map((post) => (
+                <FeedCard classes={classes}>
+                    <Post data={post} condensed />
+                </FeedCard>
+            ))}
+        </div>
         <Footer />
     </div>
 )
