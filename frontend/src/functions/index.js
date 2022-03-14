3047844353a43f88b2fdbf332db8727289e6f026
Michael Shaimerden <michael@tadesign.co.nz>
2022-03-14 20:19:36 +1300

    chore: (fe) refactor signup

--- frontend/src/functions/index.js
+++ frontend/src/functions/index.js
@@ -1,87 +1,89 @@
-import axios from "axios";
-import { SERVER_URL } from "../config";
+import axios from 'axios'
+import SERVER_URL from '../config'
 
 export const getHeaders = () => {
-  const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token')
 
-  if (!token) return null;
-  const headers = { Authorization: `Bearer ${token}` };
+    if (!token) return null
+    const headers = { Authorization: `Bearer ${token}` }
 
-  return headers;
-};
+    return headers
+}
 
 const tokenExpired = (message) => {
-  if (message === "JWT has expired") {
-    localStorage.removeItem("token");
-    localStorage.removeItem("meta");
-    window.location.reload();
-  }
-};
+    if (message === 'JWT has expired') {
+        localStorage.removeItem('token')
+        localStorage.removeItem('meta')
+        window.location.reload()
+    }
+}
 
 // NOTE: PUT YOUR IP ADDRESS HERE
 export const getData = async (url, data, jwt) => {
-  const response = await axios
-    .get(
-      `${SERVER_URL}/${url}`,
+    const response = await axios
+        .get(
+            `${SERVER_URL}/${url}`,
 
-      {
-        headers: jwt ? { Authorization: `Bearer ${jwt}` } : getHeaders(),
-        ...(data && { params: data }),
-      }
-    )
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      if (!error?.response) {
-        // in case the server goes down or something, instead of saying undefined give this error
-        throw new Error(
-          `Sorry the server is currently sleeping, come back later`
-        );
-      }
-      tokenExpired(error.response.data.message);
-      throw new Error(`${error.response.data.message}`);
-    });
-  return response;
-};
+            {
+                headers: jwt
+                    ? { Authorization: `Bearer ${jwt}` }
+                    : getHeaders(),
+                ...(data && { params: data }),
+            }
+        )
+        .then((response) => {
+            return response
+        })
+        .catch((error) => {
+            if (!error?.response) {
+                // in case the server goes down or something, instead of saying undefined give this error
+                throw new Error(
+                    `Sorry the server is currently sleeping, come back later`
+                )
+            }
+            tokenExpired(error.response.data.error)
+            throw new Error(`${error.response.data.error}`)
+        })
+    return response
+}
 
 export const postData = async (url, data = {}, jwt) => {
-  const response = await axios
-    .post(`${SERVER_URL}/${url}`, data, {
-      headers: jwt ? { Authorization: `Bearer ${jwt}` } : getHeaders(),
-    })
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      if (!error?.response) {
-        throw new Error(
-          `Sorry the server is currently sleeping, come back later`
-        );
-      }
-      tokenExpired(error.response.data.message);
-      throw new Error(`${error.response.data.message}`);
-    });
-  return response;
-};
+    const response = await axios
+        .post(`${SERVER_URL}/${url}`, data, {
+            headers: jwt ? { Authorization: `Bearer ${jwt}` } : getHeaders(),
+        })
+        .then((response) => {
+            return response
+        })
+        .catch((error) => {
+            if (!error?.response) {
+                throw new Error(
+                    `Sorry the server is currently sleeping, come back later`
+                )
+            }
+            tokenExpired(error.response.data.error)
+            throw new Error(`${error.response.data.error}`)
+        })
+    return response
+}
 
 export const patchData = async (url, data = {}) => {
-  const response = await axios
-    .patch(`${SERVER_URL}/${url}`, data, {
-      headers: getHeaders(),
-    })
-    .then((response) => {
-      return response;
-    })
-    .catch((error) => {
-      if (!error?.response) {
-        throw new Error(
-          `Sorry the server is currently sleeping, come back later`
-        );
-      }
-      tokenExpired(error.response.data.message);
+    const response = await axios
+        .patch(`${SERVER_URL}/${url}`, data, {
+            headers: getHeaders(),
+        })
+        .then((response) => {
+            return response
+        })
+        .catch((error) => {
+            if (!error?.response) {
+                throw new Error(
+                    `Sorry the server is currently sleeping, come back later`
+                )
+            }
+            tokenExpired(error.response.data.error)
 
-      throw new Error(`${error.response.data.message}`);
-    });
-  return response;
-};
+            throw new Error(`${error.response.data.error}`)
+        })
+    return response
+}
