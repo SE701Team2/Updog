b24a7a89c7d432f717c5f018c826b2b9cb6cbc9e
henrym25 <henry3399@hotmail.co.nz>
2022-03-15 13:32:05 +1300

    Fixed so page now scrolls if to view more posts

--- frontend/src/pages/dashboard/DashboardPageView.jsx
+++ frontend/src/pages/dashboard/DashboardPageView.jsx
@@ -5,7 +5,7 @@ import AddIcon from '@mui/icons-material/Add'
 import Header from '../../components/layout/header/HeaderController'
 import Footer from '../../components/layout/footer/FooterController'
 import Post from '../../components/posts/post/PostController'
-
+import classes from './dashboardpage.module.scss'
 /**
  * Card container for each feed post
  */
@@ -24,14 +24,15 @@ const FloatingAddButton = styled(Fab)({
 })
 
 const DashboardPageView = ({ feedData }) => (
-    <div>
+    <div className={classes.container}>
         <Header />
-        {feedData.map((post) => (
-            <FeedCard key={post.id}>
-                <Post data={post} condensed />
-            </FeedCard>
-        ))}
-
+        <div className={classes.pageContent}>
+            {feedData.map((post) => (
+                <FeedCard key={post.id}>
+                    <Post data={post} condensed />
+                </FeedCard>
+            ))}
+        </div>
         {/* Will need to link AddButton to post composer when completed */}
         <FloatingAddButton color="primary">
             <AddIcon />
