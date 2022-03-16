f16436f0b702ddd77566936e11e42f5262d88e08
James <54062686+nzbasic@users.noreply.github.com>
2022-03-16 20:28:49 +1300

    update routes

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,14 +1,13 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
+import { useContext, useEffect } from 'react'
 import Landing from './pages/landing/LandingController'
 import SignIn from './pages/signIn/SignInController'
 import Post from './pages/post/PostPageController'
 import Follows from './pages/follows/FollowsPageController'
 import Dashboard from './pages/dashboard/DashboardPageController'
-
 import Registration from './pages/registration/RegistrationPagesController'
 import User from './pages/user/UserPageController'
 import { AuthContext } from './contexts/AuthProvider'
-import { useContext, useEffect } from 'react'
 
 const Router = () => {
     // fetch the token to check if the user is authenticated
@@ -24,8 +23,10 @@ const Router = () => {
         routes = (
             <Routes>
                 <Route path="/post/:id" element={<Post />} />
+                <Route path="/user/:username" element={<User />} />
                 <Route path="/user/:username/follows" element={<Follows />} />
-                <Route path="/feed" element={<Dashboard />} />
+                <Route path="/" element={<Dashboard />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         )
     } else {
@@ -42,6 +43,5 @@ const Router = () => {
 
     return routes
 }
-
 
 export default Router
