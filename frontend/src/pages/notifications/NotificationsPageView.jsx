1be0d9705a2925baeb715c85d711cd9e058d3127
Bill <billsong19@hotmail.com>
2022-03-18 16:37:36 +1300

    added indent to notifpageview

--- frontend/src/pages/notifications/NotificationsPageView.jsx
+++ frontend/src/pages/notifications/NotificationsPageView.jsx
@@ -2,11 +2,11 @@ import Footer from '../../components/layout/footer/FooterController'
 import Header from '../../components/layout/header/HeaderController'
 
 const NotificationsPageView = ({ body }) => (
-    <div>
-        <Header />
-        {body}
-        <Footer />
-    </div>
+  <div>
+    <Header />
+    {body}
+    <Footer />
+  </div>
 )
 
 export default NotificationsPageView
