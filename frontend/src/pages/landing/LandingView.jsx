fcaa79b4a29803de8b7c583d72032bee0b96e9cf
James <54062686+nzbasic@users.noreply.github.com>
2022-03-18 22:45:51 +1300

    update logo

--- frontend/src/pages/landing/LandingView.jsx
+++ frontend/src/pages/landing/LandingView.jsx
@@ -1,49 +1,51 @@
 import { Link } from 'react-router-dom'
-
 import Button from '@mui/material/Button'
 import classes from './landing.module.scss'
+import Logo from '../../images/logo.png'
 
 const LandingView = () => (
-    <>
-        <div className={classes.logo}>Logo</div>
-        <div className={classes.container}>
-            <div className={classes.slogan}>
-                Check out whats going on
-                <br /> in the world right now.
-            </div>
-            <div className={classes.content}>
-                <div className={classes.buttonsContainer}>
-                    <div className={classes.loginButton}>
-                        <Link to="/signIn">
-                            <Button
-                                variant="outlined"
-                                fullWidth
-                                style={{ borderRadius: 100, padding: 10 }}
-                            >
-                                Sign in
-                            </Button>
-                        </Link>
-                    </div>
-                    <div className={classes.divider}>
-                        <hr />
-                        <span>Or</span>
-                        <hr />
-                    </div>
-                    <div className={classes.createAccount}>
-                        <Link to="/signUp">
-                            <Button
-                                variant="contained"
-                                fullWidth
-                                style={{ borderRadius: 100, padding: 10 }}
-                            >
-                                Create an Account
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  <>
+    <div className={classes.logo}>
+      <img src={Logo} alt="logo" />
+    </div>
+    <div className={classes.container}>
+      <div className={classes.slogan}>
+        Check out whats going on
+        <br /> in the world right now.
+      </div>
+      <div className={classes.content}>
+        <div className={classes.buttonsContainer}>
+          <div className={classes.loginButton}>
+            <Link to="/signIn">
+              <Button
+                variant="outlined"
+                fullWidth
+                style={{ borderRadius: 100, padding: 10 }}
+              >
+                Sign in
+              </Button>
+            </Link>
+          </div>
+          <div className={classes.divider}>
+            <hr />
+            <span>Or</span>
+            <hr />
+          </div>
+          <div className={classes.createAccount}>
+            <Link to="/signUp">
+              <Button
+                variant="contained"
+                fullWidth
+                style={{ borderRadius: 100, padding: 10 }}
+              >
+                Create an Account
+              </Button>
+            </Link>
+          </div>
         </div>
-    </>
+      </div>
+    </div>
+  </>
 )
 
 export default LandingView
