1fc3a3787ff76ab75b63239b4b1ce8f6a01ac7f6
hmit106 <68830694+hmit106@users.noreply.github.com>
2022-03-20 12:24:27 +1300

    Fix typo in LandingView.jsx

--- frontend/src/pages/landing/LandingView.jsx
+++ frontend/src/pages/landing/LandingView.jsx
@@ -4,46 +4,46 @@ import Button from '@mui/material/Button'
 import classes from './landing.module.scss'
 
 const LandingView = () => (
-    <>
-        <div className={classes.logo}>Logo</div>
-        <div className={classes.container}>
-            <div className={classes.slogan}>
-                Check out whats going on
-                <br /> in the world right now.
-            </div>
-            <div className={classes.content}>
-                <div className={classes.buttonsContainer}>
-                    <div className={classes.loginButton}>
-                        <Link to="/signIn">
-                            <Button
-                                variant="outlined"
-                                fullWidth
-                                style={{ borderRadius: 100, padding: 10 }}
-                            >
-                                Sign in
-                            </Button>
-                        </Link>
-                    </div>
-                    <div className={classes.divider}>
-                        <hr />
-                        <span>Or</span>
-                        <hr />
-                    </div>
-                    <div className={classes.createAccount}>
-                        <Link to="/signUp">
-                            <Button
-                                variant="contained"
-                                fullWidth
-                                style={{ borderRadius: 100, padding: 10 }}
-                            >
-                                Create an Account
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  <>
+    <div className={classes.logo}>Logo</div>
+    <div className={classes.container}>
+      <div className={classes.slogan}>
+        Check out what&apos;s going on
+        <br /> in the world right now.
+      </div>
+      <div className={classes.content}>
+        <div className={classes.buttonsContainer}>
+          <div className={classes.loginButton}>
+            <Link to="/signIn">
+              <Button
+                variant="outlined"
+                fullWidth
+                style={{ borderRadius: 100, padding: 10 }}
+              >
+                Sign in
+              </Button>
+            </Link>
+          </div>
+          <div className={classes.divider}>
+            <hr />
+            <span>Or</span>
+            <hr />
+          </div>
+          <div className={classes.createAccount}>
+            <Link to="/signUp">
+              <Button
+                variant="contained"
+                fullWidth
+                style={{ borderRadius: 100, padding: 10 }}
+              >
+                Create an Account
+              </Button>
+            </Link>
+          </div>
         </div>
-    </>
+      </div>
+    </div>
+  </>
 )
 
 export default LandingView
