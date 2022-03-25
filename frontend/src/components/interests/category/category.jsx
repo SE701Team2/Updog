93522eac9bea24ee057d724250bccd4bb432cbd6
Shrey Tailor <39754888+shreytailor@users.noreply.github.com>
2022-03-25 14:53:22 +1300

    Added documentation for the Category component.

--- frontend/src/components/interests/category/category.jsx
+++ frontend/src/components/interests/category/category.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import Tag from '../tag/tag'
 import classes from './category.module.css'
 
+/**
+ * This component will be used in 'Choose your interests' screen, and it can be used in the
+ * following way.
+ *
+ * For example:
+ *
+ * <Category
+ *  label="Trending"
+ *  tagArray={["Shrey", "Tailor", "SomethingElse"]}/>
+ *
+ * This will create tags for all items in the array above. Note that this component should be
+ * wrapped around the InterestsContextProvider component, because the Tag components use functions
+ * exported by that component.
+ */
 export default function Category({ label, tagArray }) {
   return (
     <div className={classes.container}>
