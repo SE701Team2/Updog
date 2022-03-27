51a5bc28e6578767239efd643a615d9c33644067
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-03-28 00:07:13 +1300

    Format rewritten by Lint Prettier

--- frontend/src/components/user/BioSetting/BioEditController.jsx
+++ frontend/src/components/user/BioSetting/BioEditController.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import BioEditView from './BioEditView'
 
-export default function BioEditController({
-    opened,
-    setOpen,
-}){
-    return(
-        <BioEditView
-            opened = {opened}
-            setOpen = {setOpen}
-        />
-    )
+export default function BioEditController({ opened, setOpen }) {
+  return <BioEditView opened={opened} setOpen={setOpen} />
 }
--- frontend/src/components/user/BioSetting/BioEditView.jsx
+++ frontend/src/components/user/BioSetting/BioEditView.jsx
@@ -1,56 +1,61 @@
 import React from 'react'
 import {
-    Button, 
-    TextField,
-    Divider, 
-    Dialog, 
-    DialogContent, 
-    Grid,
-    DialogTitle} from '@mui/material'
+  Button,
+  TextField,
+  Divider,
+  Dialog,
+  DialogContent,
+  Grid,
+  DialogTitle,
+} from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import classes from './bioEdit.module.scss'
 
+export default function BioEditView({ opened, setOpen }) {
+  return (
+    <Dialog
+      fullWidth
+      open={opened}
+      onClose={() => {
+        setOpen(false)
+      }}
+    >
+      <DialogTitle className={classes.title}>
+        Edit Bio
+        <CloseIcon
+          sx={{
+            position: 'absolute',
+            right: 20,
+            top: 20,
+          }}
+          onClick={() => {
+            setOpen(false)
+          }}
+        />
+      </DialogTitle>
 
-  export default function BioEditView({
-    opened,
-    setOpen,
-  }) {
-    return (
-        <Dialog
-            fullWidth
-            open={opened} 
-            onClose={() => {setOpen(false)
-        }}>
-        <DialogTitle className={classes.title}>
-            Edit Bio
-            <CloseIcon 
-                sx={{
-                    position: 'absolute',
-                    right:20,
-                    top:20,
-                }}
-                onClick={() => {setOpen(false)}}/>        
-        </DialogTitle>
-        
-        <DialogContent>
-            <Divider className={classes.divider} variant="middle"/>
-            <TextField
-                id="filled-multiline-static"
-                fullWidth
-                multiline
-                rows={4}
-                inputProps={{ maxLength: 150 }}
-                variant="filled"
-            />
-        </DialogContent>
-        <Grid item alignSelf="center">
-            <Button
-                className={classes.saveButton}
-                variant="contained"
-                onClick={() => {setOpen(false)}}>
-                Save
-            </Button>
-        </Grid>
-      </Dialog>
-    )
-  }
+      <DialogContent>
+        <Divider className={classes.divider} variant="middle" />
+        <TextField
+          id="filled-multiline-static"
+          fullWidth
+          multiline
+          rows={4}
+          inputProps={{ maxLength: 150 }}
+          variant="filled"
+        />
+      </DialogContent>
+      <Grid item alignSelf="center">
+        <Button
+          className={classes.saveButton}
+          variant="contained"
+          onClick={() => {
+            setOpen(false)
+          }}
+        >
+          Save
+        </Button>
+      </Grid>
+    </Dialog>
+  )
+}
