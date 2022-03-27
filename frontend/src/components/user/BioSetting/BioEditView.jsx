8722e2ab3a18f40123c20c43d8dd39e3514e288c
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-03-27 23:24:40 +1300

    Add top-right corner close icon
    For Bio edit popup, a close icon at the top right corner is implemented to further indicate users of exit

--- frontend/src/components/user/BioSetting/BioEditView.jsx
+++ frontend/src/components/user/BioSetting/BioEditView.jsx
@@ -7,12 +7,13 @@ import {
     DialogContent, 
     Grid,
     DialogTitle} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 import classes from './bioEdit.module.scss'
+
 
   export default function BioEditView({
     opened,
     setOpen,
-
   }) {
     return (
         <Dialog
@@ -20,17 +21,27 @@ import classes from './bioEdit.module.scss'
             open={opened} 
             onClose={() => {setOpen(false)
         }}>
-        <DialogTitle className={classes.title}>Edit Bio</DialogTitle>
+        <DialogTitle className={classes.title}>
+            Edit Bio
+            <CloseIcon 
+                sx={{
+                    position: 'absolute',
+                    right:20,
+                    top:20,
+                }}
+                onClick={() => {setOpen(false)}}/>        
+        </DialogTitle>
+        
         <DialogContent>
-        <Divider className={classes.divider} variant="middle" />
-        <TextField
-          id="filled-multiline-static"
-          fullWidth
-          multiline
-          rows={4}
-          inputProps={{ maxLength: 150 }}
-          variant="filled"
-        />
+            <Divider className={classes.divider} variant="middle"/>
+            <TextField
+                id="filled-multiline-static"
+                fullWidth
+                multiline
+                rows={4}
+                inputProps={{ maxLength: 150 }}
+                variant="filled"
+            />
         </DialogContent>
         <Grid item alignSelf="center">
             <Button
