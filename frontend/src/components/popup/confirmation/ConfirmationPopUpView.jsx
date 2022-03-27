664f7a87577541151e35d2887fe8f347b89e3b2d
uoa-lzha989 <68832471+uoa-lzha989@users.noreply.github.com>
2022-03-27 23:23:25 +1300

    Fixing view and scss stylings

--- frontend/src/components/popup/confirmation/ConfirmationPopUpView.jsx
+++ frontend/src/components/popup/confirmation/ConfirmationPopUpView.jsx
@@ -2,13 +2,35 @@ import { Button, Dialog, Typography } from '@mui/material'
 import classes from './confirmationPopUp.module.scss'
 
 const ConfirmationPopUpView = ({ query, method, open, onDecline }) => (
-  <Dialog open={true}>
-    <div>
-      <Typography variant="h3">{query}</Typography>
+  <Dialog open={open}>
+    <div className={classes.title}>
+      <Typography variant="h6">{query}</Typography>
     </div>
     <div className={classes.button}>
-      <Button onClick={onDecline}>No</Button>
-      <Button onClick={method}>Yes</Button>
+      <Button
+        onClick={onDecline}
+        variant="outlined"
+        sx={{
+          maxWidth: '120px',
+          maxHeight: '50px',
+          minWidth: '120px',
+          minHeight: '50px',
+        }}
+      >
+        No
+      </Button>
+      <Button
+        onClick={method}
+        variant="contained"
+        sx={{
+          maxWidth: '120px',
+          maxHeight: '50px',
+          minWidth: '120px',
+          minHeight: '50px',
+        }}
+      >
+        Yes
+      </Button>
     </div>
   </Dialog>
 )
