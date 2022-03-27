a32441bbb89d24e25f6745b5c2bf4bea1d552cc5
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-03-27 19:00:04 +1300

    [#154] Implementing People Card (SearchUserDetails) Component (#181)
    * Added files
    
    * Added basic People card without follow button
    
    * Implemented follow button
    
    * Completed follow button
    
    * Added all components
    
    * Added comments
    
    * Changed some files
    
    * Added follow button stuff
    
    * Update followButton.jsx
    
    * Rename followButton.jsx to FollowButton.jsx
    
    * Added comments
    
    * Update followbutton.module.scss
    
    * Implemented ESLint Prettier as per CONTRIBUTING.md
    
    * Made a buttons folder in components
    
    * Added View and Controller
    
    * Update SearchUserDetailsView.jsx comment according to @uoa-lzha989 's suggestion

---
+++ frontend/src/components/button/followbutton/FollowButtonView.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import classes from './followbutton.module.scss'
+
+/**
+ * The follow button component changes the Button text from following to follow or vice versa.
+ *
+ * Props:
+ * @isFollowing: a boolean that states if the user is currently following the user in the serach results.
+ * @onClick: function that would be called when the user Click to "Follow" or "UnFollow" someone
+ */
+
+export default function FollowButtonView({ isFollowing, onClick }) {
+  const [isFollowingState, setIsFollowingState] = React.useState(isFollowing)
+
+  const change = () => {
+    setIsFollowingState((boolean) => {
+      onClick(!boolean)
+      return !boolean
+    })
+  }
+
+  return (
+    <Button className={classes.followButton} onClick={change}>
+      {isFollowingState ? 'Unfollow' : 'Follow'}
+    </Button>
+  )
+}
