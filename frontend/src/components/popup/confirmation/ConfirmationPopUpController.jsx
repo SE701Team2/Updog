3a9bbbf3b85a0d93dc51033b4f4ce8e3d8758797
uoa-lzha989 <68832471+uoa-lzha989@users.noreply.github.com>
2022-03-28 00:00:44 +1300

    Renamed fields

--- frontend/src/components/popup/confirmation/ConfirmationPopUpController.jsx
+++ frontend/src/components/popup/confirmation/ConfirmationPopUpController.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import ConfirmationPopUpView from './ConfirmationPopUpView'
 
 /**
- * Creates a popup for confirmation of a given action - usually yes or no
+ * Creates a popup for confirmation of a given action.
+ * @prop question - the question being asked by the confirmation popup.
+ * @prop onAccept - method passed through from parent component for the 'Yes' button to action.
  */
 
-const ConfirmationPopUpController = ({ query, method }) => {
-  const [open, setOpen] = useState(false)
+const ConfirmationPopUpController = ({ question, onAccept }) => {
+  const [open, setOpen] = useState(true)
 
   const onDecline = () => {
     setOpen(false)
@@ -14,9 +16,9 @@ const ConfirmationPopUpController = ({ query, method }) => {
 
   return (
     <ConfirmationPopUpView
-      query={query}
-      method={method}
+      question={question}
       open={open}
+      onAccept={onAccept}
       onDecline={onDecline}
     />
   )
