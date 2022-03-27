b0886236fa312c690b4bef7f087f406a2dfa9ca1
ZHENK <chengzhenyang@gmail.com>
2022-03-27 17:57:56 +1300

    Add import/no-unresolved eslint ignore

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 import App from './App'
+// eslint-disable-next-line import/no-unresolved
 import '@fontsource/poppins'
 
 TimeAgo.addDefaultLocale(en)
