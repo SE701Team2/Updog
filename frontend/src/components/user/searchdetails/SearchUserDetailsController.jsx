79adf146b88de06179eeebfe9f4993301389f1b8
ZHENK <chengzhenyang@gmail.com>
2022-04-02 05:06:11 +1300

    Rewrite entire SearchUserDetail cause it was shit

--- frontend/src/components/user/searchdetails/SearchUserDetailsController.jsx
+++ frontend/src/components/user/searchdetails/SearchUserDetailsController.jsx
@@ -1,16 +1,26 @@
+import { useNavigate } from 'react-router'
 import SearchUserDetailsView from './SearchUserDetailsView'
 
 /**
  * Creates a search user details view i.e. component for resulting users when a search is performed.
  *
  */
-const SearchUserDetailsController = ({ username, handle, profilePic, bio }) => (
-  <SearchUserDetailsView
-    username={username}
-    handle={handle}
-    profilePic={profilePic}
-    bio={bio}
-  />
-)
+const SearchUserDetailsController = ({ user, isFollowing, handleFollow }) => {
+  const navigate = useNavigate()
+
+  const handleToProfile = () => {
+    navigate(`/user/${user.username}`)
+  }
+
+  return (
+    <SearchUserDetailsView
+      user={user}
+      bio={user.bio}
+      isFollowing={isFollowing}
+      handleFollow={handleFollow}
+      toProfile={handleToProfile}
+    />
+  )
+}
 
 export default SearchUserDetailsController
