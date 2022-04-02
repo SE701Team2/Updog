0a519d26e327785afc342e7f6f0033b772c85d01
Dhvani Shah <73872595+dshah1901@users.noreply.github.com>
2022-04-02 17:11:51 +1300

    [#200] Implementing Notification list and Notification On-Click (#225)
    Co-authored-by: Arathi Biju <62319771+arathibiju@users.noreply.github.com>
    Co-authored-by: ZHENK <chengzhenyang@gmail.com>

--- frontend/src/components/notifications/notificationcard/NotificationCardView.jsx
+++ frontend/src/components/notifications/notificationcard/NotificationCardView.jsx
@@ -1,62 +1,86 @@
-import React from 'react'
+import { React } from 'react'
 import { Avatar, Paper, Grid, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 import classes from './notificationcard.module.scss'
 
 /**
  * This component will be used in notification screen to display any new alerts received by the user. It should be
  *
  * Props:
- * @description : the message to be displayed.
+ * @type: the type of notification to be displayed. Should be either like, share, reply or follow.
  * @time : a formatted time to be displayed. Assumed to be already formatted ie in the form "3 weeks ago".
  * @handle : the handle (@) of the appropriate user
  * @username : the username of the appropriate user
  * @image : the link to the users profile image
+ * @post : the link to the relevant post
  *
  * The const variable avatar size has been extracted out in case any avatar size changes need to be made
  */
 
 const avatarSize = 70
 
-const NotificationCardView = ({
-  description,
-  time,
-  handle,
-  username,
-  image,
-}) => (
-  <Paper variant="outlined" className={classes.container}>
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-between"
-      wrap="wrap"
-      className={classes.container}
-    >
-      <Grid item className={classes.userInfo}>
-        <Grid container alignItems="center" direction="column">
-          <Avatar
-            alt={username}
-            src={image}
-            sx={{ width: avatarSize, height: avatarSize }}
-          />
-          <Typography variant="subtitle1" className={classes.username}>
-            {username}
-          </Typography>
-          <Typography variant="body2" className={classes.userHandle}>
-            @{handle}
-          </Typography>
+function NotificationCardView({ type, time, handle, username, image, post }) {
+  let content = ''
+  switch (type) {
+    case 'like':
+      content = `${username}liked your post!`
+      break
+    case 'share':
+      content = `${username}}shared your post!`
+      break
+    case 'reply':
+      content = `${username}replied to your post!`
+      break
+    case 'follow':
+      content = `${username} started following you!`
+      break
+    default:
+      content = 'Unexpected type'
+      break
+  }
+  return (
+    <Link to={`/post/${post}`} className={classes.link}>
+      <Paper variant="outlined" className={classes.container}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          wrap="wrap"
+          className={classes.container}
+        >
+          <Grid item className={classes.userInfo}>
+            <Grid container alignItems="center" direction="column">
+              <Avatar
+                alt={username}
+                src={image}
+                sx={{ width: avatarSize, height: avatarSize }}
+              />
+              <Typography variant="subtitle1" className={classes.username}>
+                {username}
+              </Typography>
+              <Typography variant="body2" className={classes.userHandle}>
+                @{handle}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            container
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography>{content}</Typography>
+          </Grid>
+          <Grid item xs="auto" container>
+            <Typography variant="caption" className={classes.timestamp}>
+              {time}
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={6} container alignItems="center" justifyContent="center">
-        <Typography>{description}</Typography>
-      </Grid>
-      <Grid item xs="auto" container>
-        <Typography variant="caption" className={classes.timestamp}>
-          {time}
-        </Typography>
-      </Grid>
-    </Grid>
-  </Paper>
-)
+      </Paper>
+    </Link>
+  )
+}
 
 export default NotificationCardView
