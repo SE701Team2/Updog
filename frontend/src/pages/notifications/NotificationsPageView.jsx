039968035c78c8d7f0ab6c4f0538eea6ff05f63e
ZHENK <chengzhenyang@gmail.com>
2022-04-02 22:17:31 +1300

    [#200] Restyle notifications to be responsive (#232)

--- frontend/src/pages/notifications/NotificationsPageView.jsx
+++ frontend/src/pages/notifications/NotificationsPageView.jsx
@@ -1,10 +1,27 @@
 import Footer from '../../components/layout/footer/FooterController'
 import Header from '../../components/layout/header/HeaderController'
+import NotificationCard from '../../components/notifications/notificationcard/NotificationCardController'
+import { FeedCard } from '../dashboard/DashboardPageView'
 
-const NotificationsPageView = ({ body }) => (
+import classes from './notificationspage.module.scss'
+
+const NotificationsPageView = ({ notifications }) => (
   <div>
     <Header />
-    {body}
+    <div className={classes.container}>
+      {notifications?.map((notification) => (
+        <FeedCard key={`${notification.from}-${notification.time}`}>
+          <NotificationCard
+            type={notification.type}
+            time={notification.time}
+            username={notification.from}
+          />
+        </FeedCard>
+      ))}
+      <p className={classes.centerText}>
+        {notifications && notifications.length === 0 && <p>No Notifcations</p>}
+      </p>
+    </div>
     <Footer />
   </div>
 )
