e8c60cc649ce43ce1c0bf4181420b40ad3929ffc
dogeliness <dogeofverymanymuchwow@gmail.com>
2022-04-02 23:20:01 +1300

    functional feed population + tests

--- backend/__tests__/activity.js
+++ backend/__tests__/activity.js
@@ -3,6 +3,15 @@ import { ActivityType } from '../enums/activity'
 import models from '../database/models'
 import Helper from './helper/helper'
 
+beforeEach(async () => {
+  await models.tags.destroy({
+    where: {},
+  })
+  await models.postTag.destroy({
+    where: {},
+  })
+})
+
 describe('Activity', () => {
   describe('getUserActivities', () => {
     it('should retrieve all user activities from newest to oldest', async () => {
@@ -40,6 +49,34 @@ describe('Activity', () => {
       const feed = await Activity.retrieveActivityFeed(following)
       expect(feed.length).toEqual(4)
       expect(feed[0].activity).toEqual(ActivityType.LIKED.type)
+    })
+  })
+  describe('retrieveInterests', () => {
+    it('should retrieve a feed of activity based on interests', async () => {
+      const u1 = await Helper.createUser()
+      const u2 = await Helper.createUser()
+
+      const p1 = await Helper.createPost('u2 post 1', u2.id)
+      const p2 = await Helper.createPost('u2 post 2', u2.id)
+
+      // Needs a long enough wait or it all happens at the same timestamp
+      await new Promise((r) => setTimeout(r, 1000))
+
+      const newTag = await Helper.createTag('Updog')
+      await Helper.createPostTag(p1.id, newTag.id)
+      await Helper.createPostTag(p2.id, newTag.id)
+      await Helper.createUserInterest(u1.id, newTag.id)
+
+      // Tests it gets interested posts
+      const interests = await Activity.getPostsForInterests(u1.id)
+      expect(interests.length).toEqual(2)
+      expect(interests[0].id).toEqual(p1.id)
+      expect(interests[1].id).toEqual(p2.id)
+
+      // Test can get feed
+      const feed = await Activity.retrieveInterests(u1.id)
+      expect(feed.length).toEqual(2)
+      expect(feed[0].activity).toEqual(ActivityType.INTERESTED.type)
     })
   })
 })
